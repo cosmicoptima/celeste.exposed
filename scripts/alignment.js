7bffee699f4e7618b27830852548d14e8b46a21a
Garcia <phosra@tutanota.com>
2023-01-02 21:25:33 -0800

    alignment.rs - rewrite JavaScript

--- scripts/alignment.js
+++ scripts/alignment.js
@@ -1,12 +1,14 @@
-rw = new WordPOS({ dictPath: "https://cdn.jsdelivr.net/npm/wordpos-web@1.0.2/dict" })
+const rw = new WordPOS({ dictPath: "https://cdn.jsdelivr.net/npm/wordpos-web@1.0.2/dict" });
 
 const generateAlignmentChart = () => {
-  for (let prefix of ["col", "row"])
-    for (let i = 1; i < 4; i++)
+  for (let prefix of ["col", "row"]) {
+    for (let i = 1; i < 4; i++) {
       rw.randAdjective(
         { count: 1 },
-        (words, _) => document.getElementById(prefix + i).textContent = words[0].replace(/_/g, " "),
-      )
+        (words, _) => document.getElementById(prefix + i).textContent = words[0].replace(/_/g, " ")
+      );
+    }
+  }
 }
 
-document.getElementById("generate-alignment").onclick = generateAlignmentChart
+document.getElementById("generate-alignment").onclick = generateAlignmentChart;
